b7482c74a0994722ab994e0317e5da6e655c78e3
Alan Wolfe <alan.wolfe@gmail.com>
2020-07-02 19:54:56 -0700

    notes

--- main.cpp
+++ main.cpp
@@ -436,7 +436,18 @@ int main2(int argc, char **argv)
 
 /*
 
+! marcos technique. include it in the analysis
 
+
+* Average and variance of error from histogram, for low sample counts unmodified algorithm. Compare vs LDS,
+* Lds as input stream? Along with as rng. both ways
+
+* unweighted reservoir sampling should have a result that is the same pdf
+
+? can we do reservoir sampling to output a color (correlation / anti correlation) as part of it's logic?
+
+* there's a thing about input pdf vs output pdf overlap (integrating the product of the 2)
+ * this controls output rate, even before screwing w/ the weighting i think.
 
 * should we use other LDSs? which ones?  Sobol? GR/Sqrt2?
 ? what other PDFs would be interesting to show?
@@ -462,11 +473,62 @@ int main2(int argc, char **argv)
 
 * Tune how many histogram buckets to use?
 
+
+------------------------- MARCOS CODE
+
+    // generate outputs which follow the PDF - Method 3
+    weightSum = 0.0f;
+    accept = 0;
+    reject = 0;
+    const int memLength = 16;
+    std::vector<float> outputs3(c_numOutputs);
+    std::vector<int> accepts3;
+    for (float input : inputs)
+    {
+        float weight = PDF(input);
+        weightSum += weight;
+        for (size_t index = 0; index < c_numOutputs; ++index)
+        {
+            if (dist_float_0_1(rng) <= weight / weightSum)
+            {
+                outputs3[index] = input;
+                accept++;
+            }
+            else
+            {
+                reject++;
+            }
+            if (accept + reject > memLength)
+            {
+                weightSum *= (float)(memLength) / (float)(1 + memLength);
+            }
+        }
+        accepts3.push_back(accept);
+    }
+
+ah yep, interesting. I need to experiment but my thought right now is that reaching a constant weight is the only way you will get a consistent histogram if you take a subsection of a stream of output.
+9:11
+that constant weight controls quality vs output sample rate.  The "right value" depends on how much quality vs output samples you want.  But, the output rate is also affected by how much the input PDF and output PDF overlap (the integration of them multiplied together).  Smaller overlap = lower output before tuning that constant weight value.  larger overlap = higher output before tuning.
+9:13
+The weight sum starting at zero and summing up to a maximum weight (in my case) may go away in the direction i'm going to be looking too, because if "consistent quality" is the desire, you'd want the start of the stream to be no different than any other part of the stream.
+9:13
+unknowns to figure out for sure, and things to verify...
+9:14
+i was also thinking it might be interesting if you didn't know / had no control over the input PDF, if maybe there could be a "temporally filtered" approximation of the input PDF, and/or just the "constant weight sum" itself, to be able to dynamically adapt to whatever the input stream was doing
+9:14
+anyways... some interesting things here i think & yeah, i'll share results. (and i will test the method you say and let you know how it does & compares!)
+)& with a constant weight sum, i want to think about how this relates to other things like perhaps rejection sampling or Russian roulette, which are very similar in formulation)
+
+
+--------------------------------------------------------------------
+
 Note:
 * unsure if I'm using LDS in ReservoirSample() correctly. we use the second value only conditionally. could be a second 1d stream maybe? not real sure.
 
 BLOG:
 * weighted reservoir sampling with replacement.
 * also just rejection sampling, as a lesser thing?
+* twitter thread on transmuting a PDF: https://twitter.com/CasualEffects/status/1277748266709508102?s=20
+* this video and the other which takes it to multiple samples: https://www.youtube.com/watch?v=A1iwzSew5QY
 
 */
